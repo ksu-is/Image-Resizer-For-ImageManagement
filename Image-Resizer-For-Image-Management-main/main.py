b5967fd9a0e83da5dcccb9dcde54147f840626b7
TFisher909 <tfishe28@students.kennesaw.edu>
2025-04-14 08:09:38 -0400

    Added the def function for process_folder_images
    Added the functionality for the code to recognize images within a file. This is the "bulk" functionality which will later allow the user to resize multiple images at once.

--- Image-Resizer-For-Image-Management-main/main.py
+++ Image-Resizer-For-Image-Management-main/main.py
@@ -19,6 +19,34 @@ def process_single_image():
     except Exception as e:
         print(f"Failed to load image: {e}")
 
+#Creates def function that allows "bulk" image recognition and the image(s) attributes
+def process_folder_images():
+    try:
+        folder_path = input ("Enter the folder containing the images: ")
+        if not os.path.isdir(folder_path):
+            print ("The folder was not found. Please try again: ")
+            return
+        image_format = ('*.png', '*.jpg', '*.jpeg')
+        image_files = []
+        for ext in image_format:
+            image_files.extend(glob.glob(os.path.join(folder_path, ext)))
+        if not image_files:
+            print ("No images found in folder.")
+            return 
+        
+        print(f"\nFound {len(image_files)} image(s): \n")
+        for file_path in image_files:
+            try:
+                img = Image.open(file_path)
+                print(f"Image: {os.path.basename(file_path)}")
+                print(f"Format: {img.format}")
+                print(f"Size: {img.size}")
+                print(f"Mode: {img.mode}")
+            except Exception as e:
+                print(f"Could not open {file_path}: {e}")
+    except Exception as e:
+        print(f"An error occured while atempting to process the folder: {e}")    
+
 
 def main():
 
